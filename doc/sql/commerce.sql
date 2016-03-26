492b98ca22c55987b85383286f00e43a6f6aa8c5
Mati Turner <matiturner@gmail.com>
2016-03-26 13:30:03 -0400

    =Added check statements and updated fields in Item

--- doc/sql/commerce.sql
+++ doc/sql/commerce.sql
@@ -8,6 +8,7 @@ CREATE TABLE Customer (
   password VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
   PRIMARY KEY(cid)
+  CHECK LEN(password) > 5
 ) ENGINE=INNODB;
 
 CREATE TABLE Staff (
@@ -23,12 +24,15 @@ CREATE TABLE Manager (
 CREATE TABLE Item (
   pid INTEGER NOT NULL AUTO_INCREMENT,
   pname VARCHAR(500) NOT NULL,
-  type VARCHAR(100) NOT NULL,
+  ptype VARCHAR(100) NOT NULL,
   cost REAL NOT NULL,
   discount REAL NOT NULL,
-  der_price REAL NOT NULL,
   quantity INTEGER NOT NULL,
-  PRIMARY KEY(pid)
+  PRIMARY KEY(pid),
+  CHECK ptype='toy' OR ptype='game' 
+  CHECK cost>0,
+  CHECK discount < 1 AND discount >=0,
+  CHECK quantity >=0
 ) ENGINE=INNODB;
 
 CREATE TABLE Orders (
@@ -39,4 +43,8 @@ CREATE TABLE Orders (
   odate DATETIME,
   FOREIGN KEY(cid) REFERENCES Customer(cid),
   FOREIGN KEY(pid) REFERENCES Item(pid)
+  CHECK cid IN (SELECT cid FROM Customer),
+  CHECK pid IN (SELECT pid FROM Item),
+  CHECK status="cart",
+  CHECK quantity > 0
 ) ENGINE=INNODB;
