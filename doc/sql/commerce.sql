f62f4cef01f61520c5cc3d605eda7a855877a32a
Matt Ruffner <matthew.ruffner@uky.edu>
2016-03-26 21:40:22 +0000

    fixed check statements, added fields to allow more granularity

--- doc/sql/commerce.sql
+++ doc/sql/commerce.sql
@@ -7,8 +7,8 @@ CREATE TABLE Customer (
   uname VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
-  PRIMARY KEY(cid)
-  CHECK LEN(password) > 5
+  PRIMARY KEY(cid),
+  CHECK (LEN(password) > 5)
 ) ENGINE=INNODB;
 
 CREATE TABLE Staff (
@@ -21,18 +21,29 @@ CREATE TABLE Manager (
   FOREIGN KEY(mid) REFERENCES Customer(cid)
 ) ENGINE=INNODB;
 
+CREATE TABLE Types (
+  tid INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  subtype VARCHAR(100),
+  about VARCHAR(1000),
+  PRIMARY KEY(tid)
+) ENGINE=INNODB;
+
 CREATE TABLE Item (
   pid INTEGER NOT NULL AUTO_INCREMENT,
   pname VARCHAR(500) NOT NULL,
   ptype VARCHAR(100) NOT NULL,
+  psubtype VARCHAR(100),
   cost REAL NOT NULL,
   discount REAL NOT NULL,
   quantity INTEGER NOT NULL,
+  image VARCHAR(100),
+  info VARCHAR(1000),
   PRIMARY KEY(pid),
-  CHECK ptype='toy' OR ptype='game' 
-  CHECK cost>0,
-  CHECK discount < 1 AND discount >=0,
-  CHECK quantity >=0
+  CHECK ((ptype IN (SELECT name FROM Types)) AND (psubtype IN (SELECT subtype FROM Types))),
+  CHECK (cost > 0),
+  CHECK (discount < 1 AND discount >=0),
+  CHECK (quantity >=0)
 ) ENGINE=INNODB;
 
 CREATE TABLE Orders (
@@ -42,9 +53,9 @@ CREATE TABLE Orders (
   quantity INTEGER NOT NULL,
   odate DATETIME,
   FOREIGN KEY(cid) REFERENCES Customer(cid),
-  FOREIGN KEY(pid) REFERENCES Item(pid)
-  CHECK cid IN (SELECT cid FROM Customer),
-  CHECK pid IN (SELECT pid FROM Item),
-  CHECK status="cart",
-  CHECK quantity > 0
+  FOREIGN KEY(pid) REFERENCES Item(pid),
+  CHECK (cid IN (SELECT cid FROM Customer)),
+  CHECK (pid IN (SELECT pid FROM Item)),
+  CHECK (status="cart"),
+  CHECK (quantity > 0)
 ) ENGINE=INNODB;
