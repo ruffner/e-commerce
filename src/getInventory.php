09208abb8d7426c55ee78291c9175f9e851d1e6f
Matt Ruffner <matthew.ruffner@uky.edu>
2016-03-29 02:37:52 +0000

    added inventory operations to inventory-card, removed unnecessary inventory-list-item. improvements to app-shell.

---
+++ src/getInventory.php
@@ -0,0 +1,29 @@
+<?php
+
+header('Content-Type: application/json');
+
+require 'defs.php';
+
+if( !isset($_POST['getInventory']) ){
+    echo json_encode( Array("result" => WRONG_PARAMS) );
+	exit;
+}
+
+require 'connect.php';
+
+$sql = "SELECT * FROM Item";
+
+$res = $_SESSION['db']->select($sql);
+
+for( $i=0; $i<count($res); $i=$i+1 ) {
+  $res[$i]['price'] = $res[$i]['cost'] - $res[$i]['cost']*$res[$i]['discount'];
+  $res[$i]['editing'] = False;
+}
+
+echo json_encode( Array(
+    "result" => "success",
+    "items" => $res
+));
+exit;
+
+?>
