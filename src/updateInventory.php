c3aa8d17769a106694161b267c6e7d1e8bf4a517
Matt Ruffner <matthew.ruffner@uky.edu>
2016-03-29 19:14:44 +0000

    added toast to notify of product edit/insert. still need sql insert/update in updateInventory.php

--- src/updateInventory.php
+++ src/updateInventory.php
@@ -5,14 +5,51 @@ header('Content-Type: application/json');
 require 'defs.php';
 
 if( !isset($_GET['itemToUpdate']) ){
-    echo json_encode( Array("result" => WRONG_PARAMS) );
+  echo json_encode( Array("result" => WRONG_PARAMS) );
 	exit;
 }
 
 require 'connect.php';
 
 
-echo json_encode( Array(
-    "result" => json_decode($_GET['itemToUpdate'])
-));
+if( isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == True) {
+  if( $_SESSION['user']['isStaff'] || $_SESSION['user']['isManager'] ) {
+    
+    $item = json_decode($_GET['itemToUpdate']);
+
+    
+    
+    if( $item->new == True ) {
+      //addItem($item);
+      
+      echo json_encode( Array(
+        "result" => UPDATE_SUCCESS,
+        "message" => UPDATE_ADD_SUCCESS_MESSAGE
+      ));
+      exit;
+    
+    } else {
+      //updateItem($item);
+      
+      echo json_encode( Array(
+        "result" => UPDATE_SUCCESS,
+        "message" => UPDATE_SUCCESS_MESSAGE
+      ));
+      exit;
+    }
+    
+  } else {
+    echo json_encode( Array(
+      "result" => ERR_NOT_ADMIN
+    ));
+    exit;
+  }
+} else {
+  echo json_encode( Array(
+    "result" => ERR_NOT_LOGGED_IN
+  ));
+  exit;
+}
+
+exit;
 ?>
