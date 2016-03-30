cb951f43a845cef475ab19143f0f809afe68cf70
Matt Ruffner <matthew.ruffner@uky.edu>
2016-03-30 17:23:29 +0000

    added login and admin check on inventory request

--- src/getInventory.php
+++ src/getInventory.php
@@ -11,21 +11,34 @@ if( !isset($_POST['getInventory']) ){
 
 require 'connect.php';
 
-$sql = "SELECT * FROM Item";
-
-$res = $_SESSION['db']->select($sql);
-
-for( $i=0; $i<count($res); $i=$i+1 ) {
-  $res[$i]['price'] = $res[$i]['cost'] - $res[$i]['cost']*$res[$i]['discount'];
-  $res[$i]['discount'] = $res[$i]['discount'] * 100;
-  $res[$i]['editing'] = False;
-  $res[$i]['icon'] = 'create';
+if( isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == True) {
+  if( $_SESSION['user']['isStaff'] || $_SESSION['user']['isManager'] ) {
+    $sql = "SELECT * FROM Item";
+    
+    $res = $_SESSION['db']->select($sql);
+    
+    for( $i=0; $i<count($res); $i=$i+1 ) {
+      $res[$i]['price'] = $res[$i]['cost'] - $res[$i]['cost']*$res[$i]['discount'];
+      $res[$i]['editing'] = False;
+      $res[$i]['icon'] = 'create';
+    }
+    
+    echo json_encode( Array(
+        "result" => "success",
+        "items" => $res
+    ));
+    exit;
+  } else {
+    echo json_encode( Array(
+      "result" => ERR_NOT_ADMIN
+    ));
+    exit;
+  }
+} else {
+  echo json_encode( Array(
+    "result" => ERR_NOT_LOGGED_IN
+  ));
+  exit;
 }
-
-echo json_encode( Array(
-    "result" => "success",
-    "items" => $res
-));
-exit;
 
 ?>
